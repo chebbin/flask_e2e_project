5a90a16b7cac79a1c0b71205847ca6d9338565e7
Chevi Ebbin <141374142+chebbin@users.noreply.github.com>
2023-12-09 23:21:47 -0500

    Create create.sql

---
+++ db/create.sql
@@ -0,0 +1,13 @@
+CREATE TABLE leading_causes_of_death (
+    year INT,
+    year INT,
+    leading_cause VARCHAR(100) NOT NULL,
+    sex VARCHAR(1) NOT NULL,
+    race_ethnicity VARCHAR(50) NOT NULL,
+    deaths INT,
+    death_rate INT,
+    age_adjusted_death_rate INT
+);
+ 
+    
+    
